35f654b566e670292ce671e5b4bd074a0454ee35
bakyrpang <bakyrpang2020@gmail.com>
2025-08-17 12:07:49 +0530

    commit 2.1 removed unnecessary files

--- whatever.py
+++
@@ -1,9 +0,0 @@
-morse = {'a':". -", 'b':"- . . .", 'c':"- . - .", 'd':"- . .", ' ':"/"}
-
-msg = input("Enter the message: ")
-new_msg = ""
-for letter in msg:
-    new_msg += morse[letter]
-    new_msg += "   "
-
-print(new_msg)
