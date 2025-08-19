496b0fb1e27226d38870d2b1550156f6e2adf54b
bakyrpang <bakyrpang2020@gmail.com>
2025-08-20 01:21:25 +0530

    commit 2.3

--- Main.py
+++ Main.py
@@ -4,12 +4,35 @@ import serial
 from math import sqrt, atan2, pi, sin, cos, radians, asin
 
 #settings
-adjust_factors = {'Hand angle': -5,
-                  'Hand distance': (0, 1),
-                  'Hand angle yz': (-90)}
+adjust_factors: dict = {'Hand angle': -5,
+                        'Hand distance': (0, 1),
+                        'Hand angle yz': (-90)}
+framerate: int = 200
+dampen_strength: dict = {'general': 20,
+                         0: 0,
+                         1: 0,
+                         2: 0,
+                         3: 0}
+#higher values can cause delayed responses. Delay(ms) = (framerate//1000)*dampen_strength
 
 #constants
-arduino = serial.Serial('COM4', 9600)
+arduino_port = 'COM3'
+
+try:
+    arduino = serial.Serial(arduino_port, 9600)
+except serial.serialutil.SerialException:
+    print("Serial port was unable to open")
+    while True:
+        input_value = input("try another port? (port/n): ")
+        if input_value == "n":
+            break
+        try:
+            arduino = serial.Serial(input_value, 9600)
+        except serial.serialutil.SerialException:
+            print("Serial port was unable to open")
+        else:
+            break
+
 webcam = cv2.VideoCapture(1)
 myHands = mediapipe.solutions.hands.Hands()
 drawingUtils = mediapipe.solutions.drawing_utils
@@ -25,7 +48,20 @@ hand_angle: float = 0
 hand_distance: float = 0
 hand_angle_yz: float = 0
 
-final_data: list = [0, 0, 0, 0]
+final_data: list = [90, 45, 52, 0]
+
+#Initialize previous values for function dampen
+prev_values: list = []
+
+for _ in range(len(final_data)):
+    prev_values.append([])
+
+for i in dampen_strength:
+    if i == 'general': continue
+    for _ in range(dampen_strength['general'] if dampen_strength[i] == 0 else dampen_strength[i]):
+        prev_values[i].append(final_data[i])
+
+print(prev_values)
 
 servoAngle: int = 0
 
@@ -69,9 +105,25 @@ def distToAngles(distance: float, angle_yz: float):
     angle2 = int(angle2)
     return angle1, angle2
 
+def dampen(x: list, previous_values: list):
+    new_values = []
+
+    #append new values of x while removing the oldest value
+    for i in range(len(x)):
+        previous_values[i].append(x[i])
+        previous_values[i].pop(0)
+
+    #take the average of the values
+    for i in range(len(x)):
+        new_values.append(sum(previous_values[i])//(dampen_strength['general'] if dampen_strength[i] == 0 else dampen_strength[i]))
+
+    #return new values and updated previous values
+    return new_values, previous_values
+
+
 while True:
     _, image = webcam.read()
-    key = cv2.waitKey(17)
+    key = cv2.waitKey(1000//framerate)
     final_data = [90,45,52,0] #reset data list
 
     #draw landmarks on the image
@@ -118,7 +170,7 @@ while True:
             hand_angle_yz = atan2(dxy, dz)
             hand_angle_yz = -(hand_angle_yz/pi)*180
             hand_angle_yz += adjust_factors['Hand angle yz']
-            hand_angle_yz = clamp(hand_angle_yz, 0, 45)
+            hand_angle_yz = clamp(hand_angle_yz, -45, 45)
 
             lenBTM = landmarks[0].y - landmarks[9].y
             lenTTM = 1-(landmarks[9].y - landmarks[12].y)
@@ -144,11 +196,11 @@ while True:
     final_data[0] = servoAngle
     final_data[1], final_data[2] = distToAngles(hand_distance, hand_angle_yz)
 
+    final_data, prev_values = dampen(final_data, prev_values)
+
     print(final_data)
 
     sendData(data=final_data, device=arduino)
-
-    previousServoAngle = servoAngle
 
 webcam.release()
 cv2.destroyWindow("Volume control")
